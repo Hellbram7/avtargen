61b642b790c33ce8a8930b1f8b33ff0c8c5cfce7
Rishabh <crishabh505@gmail.com>
2020-01-02 17:48:04 +0530

    docs

---
+++ docs/precache-manifest.fa6cc8329d7ade3358cf32a5874aa2e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c6d3e806290af4d3273bda95ebe461",
+    "url": "/avtargen/index.html"
+  },
+  {
+    "revision": "804ef22fdc94912e6782",
+    "url": "/avtargen/static/css/main.610f38e2.chunk.css"
+  },
+  {
+    "revision": "7c68d9c29ecc2dbc16cc",
+    "url": "/avtargen/static/js/2.d4b1c26b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/avtargen/static/js/2.d4b1c26b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "804ef22fdc94912e6782",
+    "url": "/avtargen/static/js/main.daf12aef.chunk.js"
+  },
+  {
+    "revision": "dee5f2a59ce1e7273d5a",
+    "url": "/avtargen/static/js/runtime-main.3b7694b1.js"
+  }
+]);
